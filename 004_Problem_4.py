5050c8edb7ca20ab557168314efebad32d5c1a50
Lalchand-M <67800296+Lalchand-M@users.noreply.github.com>
2020-07-30 22:44:59 +0530

    Update 004_Problem_4.py

--- 004_Problem_4.py
+++ 004_Problem_4.py
@@ -1,23 +1,33 @@
-#%% Imports and function declaration
+# Imports and function declaration
+
 class Group(object):
+    
     def __init__(self, _name):
+        
         self.name = _name
+        
         self.groups = []
+        
         self.users = []
 
     def add_group(self, group):
+        
         self.groups.append(group)
 
     def add_user(self, user):
+        
         self.users.append(user)
 
     def get_groups(self):
+        
         return self.groups
 
     def get_users(self):
+        
         return self.users
 
     def get_name(self):
+        
         return self.name
 
 
@@ -31,21 +41,31 @@ def is_user_in_group(user, group):
     """
 
     if user in group.get_users():  # User found
+        
         return True
+    
     else:
+        
         if len(group.get_groups()) == 0:  # Keep searching
+            
             return False
+        
         else:
+            
             for sub_group in group.get_groups():
+                
                 found = is_user_in_group(user, sub_group)
 
                 if found:
+                    
                     return True
     return False
 
 
-#%% Testing official
+# Testing official
+
 # Testing preparation
+
 parent = Group("parent")
 child = Group("child")
 sub_child = Group("subchild")
@@ -57,17 +77,29 @@ child.add_group(sub_child)
 parent.add_group(child)
 
 # Normal Cases:
+
 print('Normal Cases:')
+
 print(is_user_in_group(user='parent_user', group=parent))
+
 # False
+
 print(is_user_in_group(user='child_user', group=parent))
+
 # False
+
 print(is_user_in_group(user='sub_child_user', group=parent), '\n')
+
 # True
 
 # Edge Cases:
+
 print('Edge Cases:')
+
 print(is_user_in_group(user='', group=parent))
+
 # False
+
 print(is_user_in_group(user='', group=child))
+
 # False
