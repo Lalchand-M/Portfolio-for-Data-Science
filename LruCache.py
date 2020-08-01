ac8fde2659e08fb4f6c32cc7de2090aa17754496
Lalchand-M <67800296+Lalchand-M@users.noreply.github.com>
2020-08-01 19:27:14 +0530

    Update LruCache.py

--- LruCache.py
+++ LruCache.py
@@ -1,4 +1,4 @@
-# Imports and function declaration
+########################
 
 from collections import OrderedDict
 
@@ -15,7 +15,7 @@ class LRU_Cache(object):
 
     def get(self, key):
         
-        # Retrieve item from provided key. Return -1 if nonexistent.
+      ###########################################
 
         try:  # If value in the cache
 
@@ -33,7 +33,7 @@ class LRU_Cache(object):
 
     def set(self, key, value):
         
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+      #####################################################.
 
         if self.capacity == 0:
             
@@ -43,7 +43,7 @@ class LRU_Cache(object):
 
         if key in self.cache:  
             
-     # Update priority due to access
+     ########################################
     
             self.cache.pop(key)
         
@@ -51,7 +51,7 @@ class LRU_Cache(object):
 
         else:  
             
-        # Add to cache
+       ############################
         
             if len(self.cache) < self.capacity:  # Still space on cache
                 
@@ -64,10 +64,9 @@ class LRU_Cache(object):
                 self.cache[key] = value
 
 
-# Testing Official
+####################################
 
-# Normal Case:
-
+#########################################
 our_cache = LRU_Cache(5)
 
 our_cache.set(1, 1)
@@ -80,15 +79,14 @@ our_cache.set(4, 4)
 
 print(our_cache.get(1))
 
-# returns 1
+###################################
 
 print(our_cache.get(2))
 
-# returns 2
+###############################################
 
 print(our_cache.get(9))
-
-# returns -1 because 9 is not present in the cache
+##############################################
 
 our_cache.set(5, 5)
 
@@ -96,9 +94,7 @@ our_cache.set(6, 6)
 
 print(our_cache.get(3))
 
-# returns -1 because the cache reached it's capacity and 3 was the least recently used entry
-
-# Edge Case:
+#################################################
 
 our_cache = LRU_Cache(2)
 
@@ -110,19 +106,19 @@ our_cache.set(1, 10)
 
 print(our_cache.get(1))
 
-# should return 10
+#################################
 
 print(our_cache.get(2))
 
-# should return 2
+################################
 
 our_cache = LRU_Cache(0)
 
 our_cache.set(1, 1)
 
-# should print message "Cannot perform operation on 0 capacity cache"
+################################################
 
 print(our_cache.get(1))
 
-# should return -1
+#################################################
 
