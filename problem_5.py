2a0b590b70b35da0bf350d68309e3f81a708a869
Lalchand-M <67800296+Lalchand-M@users.noreply.github.com>
2020-08-03 10:48:15 +0530

    Update problem_5.py

--- problem_5.py
+++ problem_5.py
@@ -35,6 +35,40 @@ class Blockchain:
 
 blockchain = Blockchain()
 
+
+print(blockchain.size())
+# 0
+print(blockchain.to_list())
+# []
+
+blockchain.append('my balance: 0 | cash flow: +10 | final balance: 10')
+print(blockchain.size())
+# 1
+print(blockchain.to_list())
+# 
+# 
+
+blockchain.append('my balance: 10 | cash flow: +25 | final balance: 35')
+blockchain.append('my balance: 35 | cash flow: -15 | final balance: 20')
+blockchain.append('my balance: 20 | cash flow: +125 | final balance: 145')
+blockchain.append('my balance: 145 | cash flow: +5 | final balance: 150')
+print(blockchain.size())
+# 5
+print(blockchain.to_list())
+#
+
+# "search function"
+print(blockchain.search('my balance: 145 | cash flow: +5 | final balance: 150'))
+# 
+
+# Edge:
+print(blockchain.search('my balance: 1000 | cash flow: +100 | final balance: 1100'))
+# 
+
+blockchain = BlockChain()
+print(blockchain.search('my balance: 145 | cash flow: +5 | final balance: 150'))
+# 
+
 blockchain.add_block(1)
 print(blockchain.current_block)
 
